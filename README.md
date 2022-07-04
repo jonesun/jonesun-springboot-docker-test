944ad122df656e711d28dfa3ad8e1b84c615f5db
jone.sun <sunr922@163.com>
2022-07-04 17:50:51 +0800

    start.sh

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
 运行./gradlew clean build和./gradlew test 以此来检查项目运行是否有问题
 
-docker build -t io.github.jonesun/docker-test:0.0.1-SNAPSHOT .
+```shell
+docker build -t ccr.ccs.tencentyun.com/jonesun/docker-test:0.0.1-SNAPSHOT .
 
-docker run -p 8081:8081 io.github.jonesun/docker-test:0.0.1-SNAPSHOT
+# 后台方式
+docker run -d -p 8081:8081 ccr.ccs.tencentyun.com/jonesun/docker-test:0.0.1-SNAPSHOT docker-test
+docker run -d -p 8081:8081 --name docker-test ccr.ccs.tencentyun.com/jonesun/docker-test:0.0.1-SNAPSHOT docker-test
+
+# 先登录 我这里使用的是腾讯云的镜像仓库
+docker login --username=xxxxxx ccr.ccs.tencentyun.com
+
+# 上传镜像
+docker tag [imageId] ccr.ccs.tencentyun.com/jonesun/docker-test:[tag]
+docker push ccr.ccs.tencentyun.com/jonesun/docker-test:[tag]
+
+# 拉取镜像
+docker pull ccr.ccs.tencentyun.com/jonesun/docker-test:[tag]
+
+docker tag ccr.ccs.tencentyun.com/jonesun/docker-test:0.0.1-SNAPSHOT ccr.ccs.tencentyun.com/jonesun/docker-test:0.0.1-SNAPSHOT
+docker push ccr.ccs.tencentyun.com/jonesun/docker-test:0.0.1-SNAPSHOT
+
+./gradlew build
+docker build -t ccr.ccs.tencentyun.com/jonesun/docker-test:latest .
+docker tag ccr.ccs.tencentyun.com/jonesun/docker-test:latest ccr.ccs.tencentyun.com/jonesun/docker-test:latest
+docker push ccr.ccs.tencentyun.com/jonesun/docker-test:latest
+
+docker pull ccr.ccs.tencentyun.com/jonesun/docker-test:latest
+```
+
+
+sh start.sh
+```shell
+#!/bin/bash
+echo "脚本执行开始"
+docker pull ccr.ccs.tencentyun.com/jonesun/docker-test:latest
+
+docker stop docker-test
+docker rm docker-test
+docker run --name docker-test -d -p 8081:8081 ccr.ccs.tencentyun.com/jonesun/docker-test:latest
+#!/bin/bash
+echo "脚本执行完毕"
+```
--- src/main/java/io/github/jonesun/dockertest/controller/HelloController.java
+++ src/main/java/io/github/jonesun/dockertest/controller/HelloController.java
@@ -1,5 +1,10 @@
 package io.github.jonesun.dockertest.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.boot.info.JavaInfo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/projectInfo")
+    public StringBuilder projectInfo() {
+        StringBuilder projectInfo = new StringBuilder();
+        JavaInfo javaInfo = new JavaInfo();
+        projectInfo.append("\n=================project=================\n");
+        projectInfo.append(String.format("\nspringboot version:%s\n", SpringBootVersion.getVersion()));
+        projectInfo.append(String.format("\njavaInfo version:%s\n", javaInfo.getVersion()));
+        projectInfo.append(String.format("\njavaInfo jvm:%s_%s\n", javaInfo.getJvm().getName(), javaInfo.getJvm().getVersion()));
+        projectInfo.append(String.format("\njavaInfo runtime:%s_%s\n", javaInfo.getRuntime().getName(), javaInfo.getRuntime().getVersion()));
+        projectInfo.append(String.format("\njavaInfo vendor:%s_%s\n", javaInfo.getVendor().getName(), javaInfo.getVendor().getVersion()));
+        projectInfo.append("\n=================project=================\n");
+        logger.info(projectInfo.toString());
+        return projectInfo;
     }
 
 }
